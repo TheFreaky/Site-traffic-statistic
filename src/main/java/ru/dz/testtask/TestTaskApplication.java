bd7f5757bd9bce6b2ce4ea47d8feece0a8ea5453
Maxim <maksoh88@gmail.com>
2018-03-16 21:15:25 +0300

    configure redis

--- src/main/java/ru/dz/testtask/TestTaskApplication.java
+++ src/main/java/ru/dz/testtask/TestTaskApplication.java
@@ -2,11 +2,33 @@ package ru.dz.testtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class TestTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskApplication.class, args);
+    }
+
+    @Bean
+    public LettuceConnectionFactory jedisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplateLongValue() {
+        final RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericToStringSerializer<>(Long.class));
+        template.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+        return template;
+    }
 }
