d5e9f93a816e6242506497a3d77a08145236dc66
Maxim <maksoh88@gmail.com>
2018-03-18 20:36:00 +0300

    fix schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE public.visits (
+CREATE TABLE IF NOT EXISTS public.visits (
   id      SERIAL PRIMARY KEY,
   date    DATE NOT NULL,
   page_id VARCHAR(255) NOT NULL,
   user_id VARCHAR(255) NOT NULL
 );
 
-CREATE UNIQUE INDEX visits_date_page_id_user_id_uindex
+CREATE UNIQUE INDEX IF NOT EXISTS visits_date_page_id_user_id_uindex
   ON public.visits (date, page_id, user_id);
